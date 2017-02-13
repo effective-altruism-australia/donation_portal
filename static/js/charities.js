07cd37430e423050093152ca6cd5949e4899eda9
Ben Toner <bqp@bentoner.com>
2017-02-13 11:58:59 +1100

    move charity form logic to ractive

--- static/js/charities.js
+++ static/js/charities.js
@@ -1,12 +1,11 @@
-[
+charities = [
   {
     "name":"GiveDirectly",
     "image_top_margin":35,
     "website_image":"thumbnails/logo_givedirectly.png",
     "show_on_website":true,
     "website_label":"GiveDirectly",
-    "website_description":" makes direct, unconditional cash transfers to the world's poorest people",
-    "order_on_website":0
+    "website_description":" makes direct, unconditional cash transfers to the world's poorest people"
   },
   {
     "name":"GiveDirectly Basic income research",
@@ -14,8 +13,7 @@
     "website_image":"thumbnails/logo_gdbi.png",
     "show_on_website":true,
     "website_label":"Basic Income (GD)",
-    "website_description":" measures the impact of providing direct cash transfers to the world's poorest people over an extended period",
-    "order_on_website":0
+    "website_description":" measures the impact of providing direct cash transfers to the world's poorest people over an extended period"
   },
   {
     "name":"Deworm the World Initiative (led by Evidence Action)",
@@ -23,8 +21,7 @@
     "website_image":"thumbnails/logo_evidenceaction.gif",
     "show_on_website":true,
     "website_label":"Deworm the World",
-    "website_description":" supports programs that work to eliminate debilitating neglected tropical diseases in Africa",
-    "order_on_website":0
+    "website_description":" supports programs that work to eliminate debilitating neglected tropical diseases in Africa"
   },
   {
     "name":"Effective Altruism Australia",
@@ -32,8 +29,7 @@
     "website_image":"thumbnails/logo_eaa.png",
     "show_on_website":true,
     "website_label":"Effective Altruism Australia",
-    "website_description":" will allocate 100% of your donation to whichever of the listed charities is most in need of funding at the time",
-    "order_on_website":-1
+    "website_description":" will allocate 100% of your donation to whichever of the listed charities is most in need of funding at the time"
   },
   {
     "name":"Schistosomiasis Control Initiative",
@@ -41,7 +37,6 @@
     "website_image":"thumbnails/logo_sci.jpg",
     "show_on_website":true,
     "website_label":"Schistosomiasis Control Initiative",
-    "website_description":" supports programs that work to eliminate debilitating neglected tropical diseases in Africa",
-    "order_on_website":0
+    "website_description":" supports programs that work to eliminate debilitating neglected tropical diseases in Africa"
   }
 ]
