52f12567239adc9de124274fccd930d689384098
andrew.bird@vesparum.com <andrew.bird@vesparum.com>
2017-02-13 18:17:06 +1100

    Putting the credit card js as a callable function so it can be run by ractive when required

--- static/js/credit_card.js
+++ static/js/credit_card.js
@@ -1,62 +1,56 @@
 
-jQuery(function ($) {
+validate_cc = function () {
 
-    var $form = $('#id_pledge_form');
+        var $form = $('#id_pledge_form');
 
-    /* Fancy restrictive input formatting via jQuery.payment library*/
-    $('input[name=cardNumber]').payment('formatCardNumber');
-    $('input[name=cardCVC]').payment('formatCardCVC');
-    $('input[name=cardExpiry]').payment('formatCardExpiry');
+        /* Fancy restrictive input formatting via jQuery.payment library*/
+        $('input[name=cardNumber]').payment('formatCardNumber');
+        $('input[name=cardCVC]').payment('formatCardCVC');
+        $('input[name=cardExpiry]').payment('formatCardExpiry');
 
-    jQuery.validator.addMethod("cardExpiry", function (value, element) {
-        /* Parsing month/year uses jQuery.payment library */
-        console.log(value)
-        // value = $.payment.cardExpiryVal(value);
-        // console.log(value)
-        return this.optional(element) || /(0[1-9]|1[0-2]) \/ [0-9]{2}/.test(value);
-    }, "Invalid expiration date.");
+        jQuery.validator.addMethod("cardExpiry", function (value, element) {
+            return this.optional(element) || /(0[1-9]|1[0-2]) \/ [0-9]{2}/.test(value);
+        }, "Invalid expiration date.");
 
-    validator = $form.validate({
-        rules: {
-            cardNumber: {
-                required: true,
-                creditcard: true
+        validator = $form.validate({
+            rules: {
+                cardNumber: {
+                    required: true,
+                    creditcard: true
+                },
+                cardExpiry: {
+                    required: true,
+                    cardExpiry: true
+                },
             },
-            cardExpiry: {
-                required: true,
-                cardExpiry: true
+            highlight: function (element) {
+                $(element).closest('.form-control').removeClass('success').addClass('error');
             },
-        },
-        highlight: function (element) {
-            $(element).closest('.form-control').removeClass('success').addClass('error');
-        },
-        unhighlight: function (element) {
-            $(element).closest('.form-control').removeClass('error').addClass('success');
-        },
-        errorPlacement: function (error, element) {
-            $(element).closest('.form-group').append(error);
-        }
-    });
+            unhighlight: function (element) {
+                $(element).closest('.form-control').removeClass('error').addClass('success');
+            },
+            errorPlacement: function (error, element) {
+                $(element).closest('.form-group').append(error);
+            }
+        });
 
-    paymentFormReady = function () {
-        if (($form.find('[name=cardNumber]').hasClass("success") &&
-            $form.find('[name=cardName]').val().length > 1 &&
-            $form.find('[name=cardExpiry]').hasClass("success") &&
-            $form.find('[name=cardCVC]').val().length > 1) || ($("#id_btn_bank_transfer").hasClass('btn-primary'))){
-            return true;
-        } else {
-            return false;
-        }
-    }
+        paymentFormReady = function () {
+            if (($form.find('[name=cardNumber]').hasClass("success") &&
+                $form.find('[name=cardName]').val().length > 1 &&
+                $form.find('[name=cardExpiry]').hasClass("success") &&
+                $form.find('[name=cardCVC]').val().length > 1) || ($("#id_btn_bank_transfer").hasClass('btn-primary'))) {
+                return true;
+            } else {
+                return false;
+            }
+        };
 
-    $form.find('.subscribe').prop('disabled', true);
-    var readyInterval = setInterval(function () {
-        if (paymentFormReady()) {
-            $form.find('.subscribe').prop('disabled', false);
-            clearInterval(readyInterval);
-        }
-    }, 250)
+        $form.find('.subscribe').prop('disabled', true);
+        var readyInterval = setInterval(function () {
+            if (paymentFormReady()) {
+                $form.find('.subscribe').prop('disabled', false);
+                clearInterval(readyInterval);
+            }
+        }, 250)
 
-
-
-    });
+    };
