4886b9b402806b5362ee130373b262e278c4ca2e
Ben Toner <bqp@bentoner.com>
2017-02-16 08:43:12 +1100

    change charity order

--- static/js/charities.js
+++ static/js/charities.js
@@ -1,5 +1,29 @@
 charities = [
   {
+    "name":"Schistosomiasis Control Initiative",
+    "image_top_margin":0,
+    "website_image":"thumbnails/logo_sci.jpg",
+    "show_on_website":true,
+    "website_label":"Schistosomiasis Control Initiative",
+    "website_description":" supports programs that work to eliminate debilitating neglected tropical diseases in Africa"
+  },
+  {
+    "name":"Deworm the World Initiative (led by Evidence Action)",
+    "image_top_margin":5,
+    "website_image":"thumbnails/logo_evidenceaction.gif",
+    "show_on_website":true,
+    "website_label":"Deworm the World",
+    "website_description":" supports programs that work to eliminate debilitating neglected tropical diseases in Africa"
+  },
+  {
+    "name":"Against Malaria Foundation",
+    "image_top_margin":5,
+    "website_image":"thumbnails/logo_amf.jpg",
+    "show_on_website":true,
+    "website_label":"Against Malaria Foundation",
+    "website_description":" provides long-lasting insecticidal nets to populations at high risk of malaria, primarily in Africa"
+  },
+  {
     "name":"GiveDirectly",
     "image_top_margin":35,
     "website_image":"thumbnails/logo_givedirectly.png",
@@ -16,35 +40,11 @@ charities = [
     "website_description":" measures the impact of providing direct cash transfers to the world's poorest people over an extended period"
   },
   {
-    "name":"Deworm the World Initiative (led by Evidence Action)",
-    "image_top_margin":5,
-    "website_image":"thumbnails/logo_evidenceaction.gif",
-    "show_on_website":true,
-    "website_label":"Deworm the World",
-    "website_description":" supports programs that work to eliminate debilitating neglected tropical diseases in Africa"
-  },
-  {
-    "name":"Effective Altruism Australia",
-    "image_top_margin": 20,
-    "website_image":"thumbnails/logo_eaa.png",
-    "show_on_website":false,
-    "website_label":"Effective Altruism Australia",
-    "website_description":" will allocate 100% of your donation to whichever of the listed charities is most in need of funding at the time"
-  },
-  {
-    "name":"Schistosomiasis Control Initiative",
-    "image_top_margin":0,
-    "website_image":"thumbnails/logo_sci.jpg",
-    "show_on_website":true,
-    "website_label":"Schistosomiasis Control Initiative",
-    "website_description":" supports programs that work to eliminate debilitating neglected tropical diseases in Africa"
-  },
-  {
-    "name":"Against Malaria Foundation",
-    "image_top_margin":5,
-    "website_image":"thumbnails/logo_amf.jpg",
-    "show_on_website":true,
-    "website_label":"Against Malaria Foundation",
-    "website_description":" provides long-lasting insecticidal nets to populations at high risk of malaria, primarily in Africa"
+  "name":"Effective Altruism Australia",
+  "image_top_margin": 20,
+  "website_image":"thumbnails/logo_eaa.png",
+  "show_on_website":false,
+  "website_label":"Effective Altruism Australia",
+  "website_description":" will allocate 100% of your donation to whichever of the listed charities is most in need of funding at the time"
   }
 ]
