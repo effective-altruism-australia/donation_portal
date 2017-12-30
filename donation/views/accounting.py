8a1697ca8fc304a63202c8cc3b32f64d3396039a
Ben Toner <bqp@bentoner.com>
2017-12-30 15:23:44 +1100

    Correct credit card totals on accounting rec page. The time zone handling wasn't correct so do in different way.

--- donation/views/accounting.py
+++ donation/views/accounting.py
@@ -10,11 +10,15 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 from donation.forms import DateRangeSelector
-from donation.models import XeroReconciledDate, Account, Donation, PartnerCharity, BankTransaction, PinTransaction
+from donation.models import XeroReconciledDate, Account, Donation, PartnerCharity, BankTransaction
 
 
-def total_donations_for_partner(DonationModel, start_date, end_date, partner, after_fees=False):
-    return DonationModel.objects \
+def total_donations_for_partner(start_date, end_date, partner, payment_method=None, after_fees=False):
+    # We use the Donation view because I've thought carefully about the time zone handling for that.
+    # If you were to use the PinTransaction model directly to calculate total credit card donations,
+    # you need to think more carefully about time zones.
+    return Donation.objects \
+                .filter(**({'payment_method': payment_method} if payment_method else {})) \
                .filter(date__gte=start_date,
                        date__lt=arrow.get(end_date).shift(days=1).date(),
                        # use date_lt rather than date_lte so that it shows same-day credit card donations
@@ -63,7 +67,7 @@ def donation_counter(request):
     totals = {partner.name: (Account.objects
                              .filter(date__gte=xero_start_date, date__lte=xero_end_date, name=partner.xero_account_name)
                              .aggregate(Sum('amount'))['amount__sum'] or 0) +
-                            total_donations_for_partner(Donation, django_start_date, django_end_date, partner)
+                            total_donations_for_partner(django_start_date, django_end_date, partner)
               for partner in PartnerCharity.objects.all().order_by('name')}
 
     unknown_total = BankTransaction.objects.filter(date__gte=django_start_date, date__lte=django_end_date,
@@ -115,10 +119,11 @@ def accounting_reconciliation(request):
         end = form.fields['end'].initial
 
     totals = {partner.name:
-                  {'bank': total_donations_for_partner(BankTransaction, start, end, partner),
-                   'credit_card': total_donations_for_partner(PinTransaction, start, end, partner),
-                   'credit_card_after_fees': total_donations_for_partner(PinTransaction, start, end, partner, after_fees=True),
-                   'total': total_donations_for_partner(Donation, start, end, partner)}
+                  {'bank': total_donations_for_partner(start, end, partner, payment_method='Bank transfer'),
+                   'credit_card': total_donations_for_partner(start, end, partner, payment_method='Credit card'),
+                   'credit_card_after_fees': total_donations_for_partner(start, end, partner,
+                                                                         payment_method='Credit card', after_fees=True),
+                   'total': total_donations_for_partner(start, end, partner)}
               for partner in PartnerCharity.objects.all().order_by('name')}
 
     grand_total = {kind: sum(total[kind] for total in totals.values()) for kind in ('bank', 'credit_card',
