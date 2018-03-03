e6d97b859e6a4269adffd302d0618833ba59e2ac
Ben Toner <bqp@bentoner.com>
2018-03-04 05:06:54 +1100

    Use pypi version of pinpayments app (1/2)

---
+++ donation/templatetags/modified_pin_payment_tags.py
@@ -0,0 +1,11 @@
+# Note: the pinpayments app (which is installed) already supplies "pin_payment_tags" so this file must
+# be named differently.
+
+from django import template
+
+from pinpayments.templatetags.pin_payment_tags import pin_header, pin_form
+
+register = template.Library()
+
+register.inclusion_tag('pinpayments/modified_pin_headers.html', takes_context=True)(pin_header)
+register.inclusion_tag('pinpayments/modified_pin_form.html', takes_context=True)(pin_form)
