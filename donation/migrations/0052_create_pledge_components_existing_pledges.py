385e29b63e4d12fa10a16bf70ac88ad5e43fde94
andrew.bird@vesparum.com <andrew.bird@vesparum.com>
2018-05-09 23:28:49 +1000

    Fix fixture data for development

---
+++ donation/migrations/0052_create_pledge_components_existing_pledges.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('donation', '0050_delete_transitionaldonationsfile'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='PledgeComponent',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('amount', models.DecimalField(max_digits=12, decimal_places=2)),
+                ('partner_charity', models.ForeignKey(related_name='pledge_components', to='donation.PartnerCharity')),
+                ('pledge', models.ForeignKey(related_name='pledge_components', to='donation.Pledge')),
+            ],
+        ),
+    ]
