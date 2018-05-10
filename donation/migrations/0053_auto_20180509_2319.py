9cd513e6e879330bdbc9102ca144e602235e024d
andrew.bird@vesparum.com <andrew.bird@vesparum.com>
2018-05-10 21:16:24 +1000

    Add pledge component model, migrate existing data

---
+++ donation/migrations/0053_auto_20180509_2319.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('donation', '0052_create_pledge_components_existing_pledges'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='pledge',
+            name='recipient_org',
+            field=models.ForeignKey(to='donation.PartnerCharity', null=True),
+        ),
+        migrations.AlterUniqueTogether(
+            name='pledgecomponent',
+            unique_together=set([('pledge', 'partner_charity')]),
+        ),
+    ]
