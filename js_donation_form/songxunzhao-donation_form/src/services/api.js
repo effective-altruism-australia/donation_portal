18914e3887faa9d28c6c932c5a306eae6bc290a8
andrew.bird@vesparum.com <andrew.bird@vesparum.com>
2018-05-29 22:41:08 +1000

    Update partner charities list in API.js to match endpoint

--- js_donation_form/songxunzhao-donation_form/src/services/api.js
+++ js_donation_form/songxunzhao-donation_form/src/services/api.js
@@ -2,52 +2,24 @@ import React from "react";
 
 export default class APIService {
     getCharities() {
-        const charities = [
-            {
-                name: "Schistosomiasis Control Initiative",
-                blurb: <span><b>Schistosomiasis Control Initiative</b> supports programs that work
-                    to eliminate debilitating neglected tropical diseases in Africa</span>,
-                logo: "//donations.effectivealtruism.org.au/static/thumbnails/logo_sci.jpg",
-            },
-            {
-                name: "Deworm the World",
-                blurb: <span><b>Deworm the World</b> supports programs that work to eliminate debilitating neglected
-                    tropical diseases in Africa</span>,
-                logo: "//donations.effectivealtruism.org.au/static/thumbnails/logo_evidenceaction.gif",
-            },
-            {
-                name: "Against Malaria Foundation",
-                blurb: <span><b>Against Malaria Foundation</b> provides long-lasting insecticidal nets to populations
-                    at high risk of malaria, primarily in Africa.</span>,
-                logo: "//donations.effectivealtruism.org.au/static/thumbnails/logo_amf.jpg",
-                directDonationOnly: true,
-                advice: <p className="donation-info">
-                    Donations to the Against Malaria Foundation are tax deductible in Australia.
-                    We prefer that you donate to the Against Malaria Foundation directly at&nbsp;
-                    <a href='//www.againstmalaria.com/Donation.aspx?GroupID=86' target="_blank" rel="noopener noreferrer">
-                        https://www.againstmalaria.com/Donation.aspx?GroupID=86
-                    </a>.
-                </p>
-            },
-            {
-                name: "GiveDirectly",
-                blurb: <span><b>GiveDirectly</b> makes direct, unconditional cash transfers to
-                    the world's poorest people</span>,
-                logo: "//donations.effectivealtruism.org.au/static/thumbnails/logo_givedirectly.png"
-            },
-            {
-                name: "Basic Income (GD)",
-                blurb: <span><b>Basic Income (GD)</b> measures the impact of providing direct cash transfers to the world's
-                    poorest people over an extended period</span>,
-                logo: "//donations.effectivealtruism.org.au/static/thumbnails/logo_gdbi.png"
-            },
-            {
-                name: "Malaria Consortium",
-                blurb: <span><b>Malaria Consortium</b> supports programs that
-                    provide malaria prevention medication to children in sub-Saharan Africa</span>,
-                logo: "//donations.effectivealtruism.org.au/static/thumbnails/logo_mc.jpg",
-            }
-        ];
+
+        const charities = [{
+            'slug_id': 'sci',
+            'name': 'Schistosomiasis Control Initiative'
+        }, {
+            'slug_id': 'give_directly_basic',
+            'name': 'GiveDirectly Basic income research'
+        }, {
+            'slug_id': 'give_directly',
+            'name': 'GiveDirectly'
+        }, {
+            'slug_id': 'malaria_consortium',
+            'name': 'Malaria Consortium'
+        }, {
+            'slug_id': 'deworm',
+            'name': 'Deworm the World Initiative (led by Evidence Action)'
+        }];
+
         return Promise.resolve(charities);
     }
 
@@ -67,6 +39,17 @@ export default class APIService {
     }
 
     submit(data) {
+        console.log(data);
+
+        fetch('http://127.0.0.1:8000/pledge', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data)
+        });
+
         return Promise.resolve({
             'bank_reference': 'xxxx',
             'receipt_url': 'xxxx'
