3bf4e46d0bca455958301514127499c0f4e43106
andrew.bird@vesparum.com <andrew.bird@vesparum.com>
2018-06-05 22:00:09 +1000

    Neaten up the format of data posted by API. fixes #51

--- react/apps/donation_form/services/api.js
+++ react/apps/donation_form/services/api.js
@@ -16,13 +16,48 @@ export default class APIService {
     submit(data) {
         console.log(data);
 
-        fetch('/pledge_new', {
+        let donation = data.donation;
+
+        if (donation.mode === 'auto') {
+            console.log('hi');
+            let amount = null;
+            if (donation.amount.preset === 'other') {
+                amount = donation.amount.value;
+            } else {
+                amount = donation.amount.preset;
+            }
+
+            let charity = 'unallocated';
+            if (data.charity) {
+                charity = data.charity.slug_id
+            }
+            donation.components = [{'charity': charity, 'amount': amount}];
+        } else if (donation.mode === 'custom') {
+            donation.components = [];
+            for (let charity in donation.amount) {
+                if (charity === 'preset') {
+                    continue;
+                }
+                donation.components.push({'charity': charity, 'amount': donation.amount[charity]});
+            }
+        }
+
+        if (donation.will_contribute){
+            donation.components.push({'charity': 'eaa', 'amount': donation.contribute.value});
+            delete donation.will_contribute;
+            delete donation.contribute;
+        }
+        delete donation.amount;
+
+        console.log(donation);
+
+        fetch('/pledge_new/', {
             method: 'POST',
             headers: {
                 'Accept': 'application/json',
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify(data)
+            body: JSON.stringify(donation)
         });
 
         return Promise.resolve({
