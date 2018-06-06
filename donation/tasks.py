93b0a44b2d568b88710ac21cac678a3785a7b320
andrew.bird@vesparum.com <andrew.bird@vesparum.com>
2018-06-07 03:40:33 +1000

    Use celery to send bank transfer email so the user gets a much faster response

--- donation/tasks.py
+++ donation/tasks.py
@@ -1,7 +1,14 @@
 from donation_portal.eaacelery import app
+from donation.models import Pledge
 
 from .eaaxero import import_bank_transactions, import_trial_balance as import_trial_balance_non_delayed
 import reporting
+from .emails import send_bank_transfer_instructions
+
+
+@app.task()
+def send_bank_transfer_instructions_task(pledge_id):
+    send_bank_transfer_instructions(Pledge.objects.get(id=pledge_id))
 
 
 @app.task()
