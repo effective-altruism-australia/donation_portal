c2c36d80d6db94c0efa987414461948037a0332f
andrew.bird@vesparum.com <andrew.bird@vesparum.com>
2018-06-12 16:46:12 +1000

    Format card number as user types it in

--- react/apps/donation_form/pages/donation-form/card-payment.js
+++ react/apps/donation_form/pages/donation-form/card-payment.js
@@ -11,7 +11,20 @@ class CardPaymentDetails extends Component {
 
     render() {
         const normalizeCardNumber = (value) => {
-            return value.replace(/\s/g, '');
+            var v = value.replace(/\s+/g, '').replace(/[^0-9]/gi, '');
+            var matches = v.match(/\d{4,16}/g);
+            var match = matches && matches[0] || '';
+            var parts = [];
+
+            for (let i = 0, len = match.length; i < len; i += 4) {
+                parts.push(match.substring(i, i + 4))
+            }
+
+            if (parts.length) {
+                return parts.join(' ')
+            } else {
+                return value
+            }
         };
         const normalizeExpiry = (value) => {
             return value.replace(
