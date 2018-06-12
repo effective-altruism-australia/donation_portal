d4816c7095e6078910eb3d591adf5b1bc4ce4729
andrew.bird@vesparum.com <andrew.bird@vesparum.com>
2018-06-12 19:39:26 +1000

    Reduce max length on name fields from 1000 to 100

---
+++ donation/migrations/0067_auto_20180612_1937.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+def trim_names(apps, schema_editor):
+    Pledge = apps.get_model('donation', 'Pledge')
+    for pledge in Pledge.objects.all():
+        pledge.first_name = pledge.first_name[0:100]
+        pledge.last_name = pledge.last_name[0:100]
+        pledge.save()
+
+
+def reverse(apps, schema_editor):
+    pass
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ('donation', '0066_auto_20180611_2210'),
+    ]
+
+    operations = [
+        migrations.RunPython(trim_names, reverse_code=reverse),
+        migrations.AlterField(
+            model_name='pledge',
+            name='first_name',
+            field=models.CharField(max_length=100, verbose_name='name', blank=True),
+        ),
+        migrations.AlterField(
+            model_name='pledge',
+            name='last_name',
+            field=models.CharField(max_length=100, blank=True),
+        ),
+    ]
--- react/apps/donation_form/services/validation.js
+++ react/apps/donation_form/services/validation.js
@@ -3,6 +3,7 @@ export const required = value => (value ? undefined : 'The field is required');
 export const maxLength = max => value =>
     value && value.length > max ? `Must be ${max} characters or less` : undefined;
 export const maxLength15 = maxLength(15);
+export const maxLength100 = maxLength(100);
 export const minLength = min => value =>
     value && value.length < min ? `Must be ${min} characters or more` : undefined;
 export const minLength2 = minLength(2);
