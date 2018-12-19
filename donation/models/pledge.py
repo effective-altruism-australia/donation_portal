a9e97277c6b6da150f915584c8a226e708ebfebf
Ben Toner <bqp@bentoner.com>
2018-12-20 06:22:18 +1100

    Revert "edit receipt text. fixes #74". There's an easier way.
    This reverts commit c010f237bc0b084dc6ae7fb585ec12181b850370.

--- donation/models/pledge.py
+++ donation/models/pledge.py
@@ -79,8 +79,7 @@ class Pledge(models.Model):
     def partner_charity_str(self):
         num_components = self.components.count()
         if num_components == 1:
-            name = self.components.get().partner_charity.name
-            return "our most effective partner charities, based on evidence and need" if name == "Unallocated" else name
+            return self.components.get().partner_charity.name
         elif num_components > 1:
             partner_names = [component.partner_charity.name for component in self.components.all()]
             return '{} and {}'.format(', '.join(partner_names[:-1]), partner_names[-1])
