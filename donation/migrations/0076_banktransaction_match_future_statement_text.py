e5a623b31f6ebd0a2ceb00ea051772efa4b066a0
Andrew Bird <andrew.bird@vesparum.com>
2019-02-06 20:24:29 -0800

    Fix migrations

--- donation/migrations/0076_banktransaction_match_future_statement_text.py
+++ donation/migrations/0076_banktransaction_match_future_statement_text.py
@@ -7,11 +7,11 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('donation', '0071_auto_20190207_1507'),
+        ('donation', '0075_donation_component_view'),
     ]
 
     operations = [
-        migrations.AlterField(
+        migrations.AddField(
             model_name='banktransaction',
             name='match_future_statement_text',
             field=models.BooleanField(default=False, help_text='Tick this box if we should link all future transactions with this text to the same pledge. This should only be ticked if the text is reasonably unique. e.g. Do NOT tick it if the text is "donation", or "GiveDirectly" etc.'),
