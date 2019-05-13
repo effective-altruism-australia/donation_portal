3630b47df70396071eb03bcd7a72a2bc9c380395
Andrew Bird <andrew.bird@vesparum.com>
2019-05-13 03:33:58 -0700

    Remove URL patterns for pledge form since we were getting some weird activity on these pages

--- donation_portal/urls.py
+++ donation_portal/urls.py
@@ -34,8 +34,8 @@ urlpatterns = [
     # Donations
     # url(r'^pledge/([0-9])/$', pledge, name='pledge')
     url(r'^pledge.js', PledgeJS.as_view(), name='pledge-js'),
-    url(r'^pledge_new/', PledgeView.as_view(), name='pledge-new'),
-    url(r'^pledge/', PledgeViewOld.as_view(), name='pledge'),
+    # url(r'^pledge_new/', PledgeView.as_view(), name='pledge-new'),
+    # url(r'^pledge/', PledgeViewOld.as_view(), name='pledge'),
 
     url(r'^receipt/(?P<pk>[0-9]+)/(?P<secret>[a-zA-Z0-9]+)', download_receipt, name='download-receipt'),
     url(r'^paypal/', include('paypal.standard.ipn.urls')),
