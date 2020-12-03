8458a9759f90b2abbf5744d5829baff41d68a07e
Andrew Bird <andrew.bird@vesparum.com>
2020-12-04 10:01:59 +1100

    Clean up

--- donation/views/stripe_checkout.py
+++ donation/views/stripe_checkout.py
@@ -12,26 +12,20 @@ stripe.api_key = settings.STRIPE_API_KEY
 @csrf_exempt
 @require_POST
 def create_checkout_session(request):
-    print(type(request.body))
-    # print(request.body, request.data)
     data = json.loads(request.body)
-    print(data)
     line_items = []
-    try:
-        for slug, amount in data['donations'].items():
-            line_items.append(
-                {'price_data': {'currency': 'aud', 'product': PartnerCharity.objects.get(slug_id=slug).stripe_product_id,
-                                'unit_amount': amount, 'recurring': {'interval': 'month'} if data['recurring'] else None},
-                 'quantity': 1, }
-            )
-        session = stripe.checkout.Session.create(
-            payment_method_types=['card'],
-            line_items=line_items,
-            mode='subscription' if data['recurring'] else 'payment',
-            success_url='https://example.com/success',
-            cancel_url='https://example.com/cancel',
+    for slug, amount in data['donations'].items():
+        line_items.append(
+            {'price_data': {'currency': 'aud', 'product': PartnerCharity.objects.get(slug_id=slug).stripe_product_id,
+                            'unit_amount': amount * 100, 'recurring': {'interval': 'month'} if data['recurring'] else None},
+             'quantity': 1, }
         )
-    except Exception as e:
-        print(e)
+    session = stripe.checkout.Session.create(
+        payment_method_types=['card'],
+        line_items=line_items,
+        mode='subscription' if data['recurring'] else 'payment',
+        success_url='https://example.com/success',
+        cancel_url='https://example.com/cancel',
+    )
 
     return JsonResponse({'id': session.id})
