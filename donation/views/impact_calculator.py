62520813e980848ec41281c9970f7265bc16dd79
Andrew Bird <andrew.bird@eaa.org.au>
2021-05-19 04:43:46 +0000

    allow iframe embed

--- donation/views/impact_calculator.py
+++ donation/views/impact_calculator.py
@@ -6,8 +6,13 @@ from django.views.generic import View
 
 from donation.models import PartnerCharity, ReferralSource
 from django.views.generic.base import TemplateView
+from django.views.decorators.clickjacking import xframe_options_exempt
 
 
 class ImpactCalculator(TemplateView):
 
     template_name = "impact.html"
+
+    @xframe_options_exempt
+    def get(self, request):
+        return super().get(request)
