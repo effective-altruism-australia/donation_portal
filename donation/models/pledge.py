99fe20b5bcc0744d623e4e2415b4f541fc730fd6
Andrew Bird <andrew.bird@eaa.org.au>
2021-05-21 07:23:38 +0000

    wip

--- donation/models/pledge.py
+++ donation/models/pledge.py
@@ -118,7 +118,7 @@ class Pledge(models.Model):
 
     @property
     def donor_portal(self):
-        return reverse('donor-portal', kwargs={'customer_id': self.stripe_customer_id'})
+        return reverse('donor-portal', kwargs={'customer_id': self.stripe_customer_id})
 
     def __unicode__(self):
         components = ', '.join([c.__unicode__() for c in self.components.all()])
