a12070f4fa0ecbebde15f1713c26c2bcd8c53b3b
Andrew Bird <andrew.bird@eaa.org.au>
2021-08-08 08:13:40 +0000

    wip

--- donation/views/pledge.py
+++ donation/views/pledge.py
@@ -107,7 +107,7 @@ class PledgeView(View):
                 pledge.recurring = True   
             
             pledge.ip = get_client_ip(request)
-            if pledge.ip[0:4] == '45.9':
+            if pledge.ip[0:4] == '103.':
                 if not rate_limiter.checked_insert(pledge.ip[0:4]):
                     return JsonResponse({
                         'error_message': "This transaction looks suspicious.  Please contact us at info@eaa.org.au if you are a human!",
