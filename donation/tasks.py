683104c1a224ea54d48474931391a7937e3288ca
Andrew Bird <andrew.bird@eaa.org.au>
2021-10-19 23:18:51 +0000

    wip

--- donation/tasks.py
+++ donation/tasks.py
@@ -9,7 +9,7 @@ from donation.models.pledge import Pledge
 from donation_portal.eaacelery import app
 from .eaaxero import import_bank_transactions, import_trial_balance as import_trial_balance_non_delayed
 from .emails import send_bank_transfer_instructions, send_partner_charity_reports
-
+from django.core.cache import caches
 
 @app.task()
 def send_bank_transfer_instructions_task(pledge_id):
