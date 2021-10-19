600b2b5debaf26094e685a93a08c7d5cc0b027c2
Andrew Bird <andrew.bird@eaa.org.au>
2021-10-19 23:02:20 +0000

    wip

--- donation/views/xero_views.py
+++ donation/views/xero_views.py
@@ -22,7 +22,7 @@ def process_callback_view(request):
     cred_state = caches['default'].get('xero_creds')
     credentials = OAuth2Credentials(**cred_state)
     print(request.META)
-    auth_secret = request.META['RAW_URI']
+    auth_secret = 'https//' + request.META['HTTP_HOST'] + request.META['RAW_URI']
     credentials.verify(auth_secret)
     credentials.set_default_tenant()
     caches['default'].set('xero_creds', credentials.state)
