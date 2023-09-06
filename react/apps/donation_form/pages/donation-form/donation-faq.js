60fa408f3974a6a88bcc2cf06c1be39825ebf879
Nathan Sherburn <nathansherburn@users.noreply.github.com>
2023-09-06 13:01:08 +1000

    Update donation-faq.js
    Prevent FAQs opening in the iframe

--- react/apps/donation_form/pages/donation-form/donation-faq.js
+++ react/apps/donation_form/pages/donation-form/donation-faq.js
@@ -5,7 +5,7 @@ export default function DonationFaq(props) { return (
       <h2>Question about your donation?</h2>
       <p>
         Head over to our{" "}
-        <a href="https://effectivealtruism.org.au/faq/">FAQ section</a> for more
+        <a target="_parent" href="https://effectivealtruism.org.au/faq/">FAQ section</a> for more
         information on donating, tax, receipts, accepted credit cards and more.
       </p>
     </div>
