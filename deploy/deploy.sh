2ae3f46ad6ee7b6872563602758f8eb74433e1ce
Andrew Bird <andrew.bird@eaa.org.au>
2024-06-05 01:34:56 +0000

    Initial deploy script

---
+++ deploy/deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Navigate to the project directory
+cd /home/eaa/donation_portal
+
+# Pull the latest changes from the repository
+git pull origin
+
+echo "Deployment completed successfully."
