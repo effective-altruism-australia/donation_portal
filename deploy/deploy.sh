5d1e4d97310edd0666cee3affbaac98d553b6a09
Andrew Bird <andrew.bird@eaa.org.au>
2024-06-05 01:47:47 +0000

    Add to deploy script

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -6,11 +6,25 @@ sudo -u eaa -H bash << EOF
 echo "Logged in successfully."
 
 # Navigate to the project directory
-cd /home/eaa/donation_portal
+cd /home/eaa/donation_portal/react
 
 # Pull the latest changes from the repository
 git pull
 
-echo "Deployment completed successfully."
+echo "Building frontend."
+npm run build
+
+cd /home/eaa/donation_portal
+
+echo "Collecting staticfiles."
+./manage.py collectstatic
+
+echo "Migrating db."
+./manage.py migrate
 
 EOF
+
+echo "Restarting service."
+sudo supervisorctl restart donations:
+
+echo "Deployment completed successfully."
