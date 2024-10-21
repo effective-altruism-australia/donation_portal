5ae48a93399b70d95a27fe462833b8d23738a8f0
Nathan Sherburn <nathansherburn@gmail.com>
2024-10-21 06:02:57 +0000

    Remove salt_settings.py and add .env file

--- donation_portal/settings.py
+++ donation_portal/settings.py
@@ -12,11 +12,28 @@ import datetime
 import os
 
 from celery.schedules import crontab
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Helper function to get environment variables with defaults and casting
+def get_env(name, default=None, cast=str):
+    value = os.environ.get(name, default)
+    if value is None:
+        return None
+    
+    if cast == bool:
+        return value.lower() in ('true', 't', 'yes', 'y', '1')
+    
+    try:
+        return cast(value)
+    except ValueError:
+        return default
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Application definition
-
 INSTALLED_APPS = (
     'django.contrib.admin',
     'django.contrib.auth',
@@ -69,7 +86,6 @@ WSGI_APPLICATION = 'donation_portal.wsgi.application'
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
-
 LANGUAGE_CODE = 'en-au'
 TIME_ZONE = 'Australia/Melbourne'
 USE_I18N = True
@@ -78,7 +94,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
-
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "tmp/static")
 STATICFILES_DIRS = (
@@ -88,9 +103,7 @@ STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesSto
 
 # Celery
 # http://docs.celeryproject.org/en/latest/configuration.html
-
 # To run Celery: celery -A donation_portal worker --beat -l INFO
-
 # We'll run this every 4 hours in case xero is late importing transactions or something
 CELERYBEAT_SCHEDULE = {
     'process-transactions': {
@@ -108,81 +121,129 @@ CELERYBEAT_SCHEDULE = {
         'schedule': crontab(minute=0, hour=5, day_of_week='1')
     }
 }
+BROKER_URL = 'redis://localhost:6379'
+
+# Redis
+REDIS_HOST = 'localhost'
+REDIS_PORT = '6379'
 
 LOGIN_URL = '/admin/login'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-PAYPAL_TEST = True
-
 CREDIT_CARD_RATE_LIMIT_MAX_TRANSACTIONS = 6
 CREDIT_CARD_RATE_LIMIT_PERIOD = 12 * 60 * 60
 CREDIT_CARD_RATE_LIMIT_ENABLED = True
 
-TESTING_EMAIL = "@".join(['andrewbirdemail', 'gmail.com'])
+TESTING_EMAIL = get_env('TESTING_EMAIL')
 EAA_INFO_EMAIL = 'info@eaa.org.au'
 
-DEBUG = False
+# Debug mode (do *not* set to True in production)
+DEBUG = get_env('DEBUG', False, bool)
 
-# Deployment
+# Secret key used to provide cryptographic signing
+SECRET_KEY = get_env('SECRET_KEY')
 
-# Settings configured by Salt
-if os.path.exists(os.path.join(os.path.dirname(__file__), "salt_settings.py")):
-    # noinspection PyUnresolvedReferences
-    from salt_settings import *  # NOQA
-else:
-    from salt_settings_example import *  # NOQA
+# Security settings
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+CSRF_COOKIE_SECURE = get_env('CSRF_COOKIE_SECURE', True, bool)
+SESSION_COOKIE_SECURE = get_env('SESSION_COOKIE_SECURE', True, bool)
+CORS_ORIGIN_WHITELIST = os.environ.get('CORS_ORIGIN_WHITELIST', '').split(',')
 
-# Custom configuration settings
-if os.path.exists(os.path.join(os.path.dirname(__file__), "local_settings.py")):
-    # noinspection PyUnresolvedReferences
-    from local_settings import *  # NOQA
+# Database
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': get_env('POSTGRES_DB_NAME'),
+        'USER': get_env('POSTGRES_USER'),
+        'PASSWORD': get_env('POSTGRES_PASSWORD'),
+        'HOST': get_env('POSTGRES_HOST'),
+        'PORT': get_env('POSTGRES_PORT'),
+    }
+}
+
+# Email (Postmark)
+EMAIL_BACKEND = 'postmark.django_backend.EmailBackend'
+POSTMARK_API_KEY = get_env('POSTMARK_API_KEY')
+POSTMARK_SENDER = get_env('POSTMARK_SENDER')
+POSTMARK_TEST_MODE = get_env('POSTMARK_TEST_MODE', False, bool)
+POSTMARK_TRACK_OPENS = get_env('POSTMARK_TRACK_OPENS', False, bool)
+
+# Email (MailChimp)
+MAILCHIMP_API_KEY = get_env('MAILCHIMP_API_KEY')
+EA_NEWSLETTER_MAILCHIMP_API_KEY = get_env('EA_NEWSLETTER_MAILCHIMP_API_KEY')
+
+# Xero
+XERO_RSA_KEY = get_env('XERO_RSA_KEY')
+XERO_CONSUMER_KEY = get_env('XERO_CONSUMER_KEY')
+XERO_INCOMING_ACCOUNT_ID_DICT = {
+    "eaa": get_env('XERO_INCOMING_ACCOUNT_ID_EAA'), 
+    "eaae": get_env('XERO_INCOMING_ACCOUNT_ID_EAAE')
+}
+XERO_INCOMING_ACCOUNT_ID = get_env('XERO_INCOMING_ACCOUNT_ID_EAA')
+XERO_DAYS_TO_IMPORT = 300
+XERO_CLIENT_ID = get_env('XERO_CLIENT_ID')
+XERO_CLIENT_SECRET = get_env('XERO_CLIENT_SECRET')
+TENANT_IDS = {
+    'eaa': get_env('XERO_TENANT_ID_EAA'),
+    'eaae': get_env('XERO_TENANT_ID_EAAE')
+}
+
+# Stripe
+STRIPE_API_KEY = get_env('STRIPE_API_KEY_EAA')
+STRIPE_API_KEY_DICT = {
+    "eaa": get_env('STRIPE_API_KEY_EAA'), 
+    "eaae": get_env('STRIPE_API_KEY_EAAE')
+}
+
+# Pinpayments
+PIN_ENVIRONMENTS = {"No longer used"}
+
+# Sentry
+ENABLE_SENTRY = get_env('ENABLE_SENTRY', True, bool)
 
 if ENABLE_SENTRY:
     INSTALLED_APPS += ('raven.contrib.django.raven_compat',)
+    import raven
+    release = None
+    try:
+        release = raven.fetch_git_sha(os.path.dirname(__file__))
+    except:
+        pass
+
+    SENTRY_DSN = get_env('SENTRY_DSN')
+    SENTRY_PUBLIC_DSN = get_env('SENTRY_PUBLIC_DSN')
+    RAVEN_CONFIG = {
+        'dsn': SENTRY_DSN,
+        'release': release
+    }
+SENTRY_PUBLIC_DSN = get_env('SENTRY_PUBLIC_DSN')
 
 # Date from which to start sending receipts automatically (inclusive)
 AUTOMATION_START_DATE = datetime.date(2016, 10, 19)
 
-CORS_ORIGIN_WHITELIST = (
-    'donations.effectivealtruism.org.au',
-    'effectivealtruism.org.au',
-    'eaa.org.au',
-    'dev.effectivealtruism.org.au',
-    'localhost:8000'
-)
-
-#########
 # Webpack
-#########
-
 STATICFILES_DIRS = STATICFILES_DIRS + (
     os.path.join(BASE_DIR, 'react', 'build'),
     os.path.join(BASE_DIR, 'react', 'public'),
 )
-
 WEBPACK_LOADER = {
     'DEFAULT': {
         'BUNDLE_DIR_NAME': 'bundle/',
-
         'STATS_FILE': os.path.join(BASE_DIR, 'react/build/webpack-stats.json'),
-
         # This disables polling in production. We assume the bundles are built and stay unchanged while the application is running.
         'CACHE': not DEBUG,
     }
 }
 
 CACHES = {
-     'default': {
-         'BACKEND':
-         'django.core.cache.backends.memcached.MemcachedCache',
-         'LOCATION': '127.0.0.1:11211',
-         'TIMEOUT': 60 * 60 * 60 * 24,
-         'OPTIONS': {
-             'MAX_ENTRIES': 5000,
-         }
-     } }
-
-PIN_ENVIRONMENTS = {"dummy"}
-
-# vim: cc=80 tw=79 ts=4 sw=4 sts=4 et sr
+    'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        'LOCATION': '127.0.0.1:11211',
+        'TIMEOUT': 60 * 60 * 60 * 24,
+        'OPTIONS': {
+            'MAX_ENTRIES': 5000,
+        }
+    }
+}
