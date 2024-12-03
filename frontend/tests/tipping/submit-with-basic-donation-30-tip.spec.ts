c30af5fe08246e1c608c853c1d2035705ecf8275
Nathan Sherburn <nathansherburn@gmail.com>
2024-12-03 11:27:12 +1100

    Add tests for tipping

---
+++ frontend/tests/tipping/submit-with-basic-donation-30-tip.spec.ts
@@ -0,0 +1,64 @@
+import { expect, test } from "@playwright/test";
+
+/*
+Ensure that the form sends the right data when a basic donation with a 30% tip is submitted.
+*/
+
+test("Tipping: submit with basic donation and 30% tip", async ({ page }) => {
+  await page.goto("http://localhost:8000/pledge_new/");
+
+  await page.getByText("$50").click();
+
+  await page.getByText("30%").click();
+
+  await page.getByLabel("First name").fill("Nathan");
+
+  await page.getByLabel("Last name").fill("Sherburn");
+
+  await page.getByLabel("Email", { exact: true }).fill("testing@eaa.org.au");
+
+  await page.getByLabel("Postcode").fill("3000");
+
+  await page.locator("#communications-section--referral-sources").selectOption("cant-remember");
+
+  const testFinished = new Promise<void>((resolve) => {
+    page.on("request", async (request) => {
+      if (request.url().includes("pledge_new")) {
+        let data = JSON.parse(request.postData() || "{}");
+        expect(data["payment_method"]).toBe("credit-card");
+        expect(data["recurring_frequency"]).toBe("one-time");
+        expect(data["recurring"]).toBe(false);
+        expect(data["first_name"]).toBe("Nathan");
+        expect(data["last_name"]).toBe("Sherburn");
+        expect(data["email"]).toBe("testing@eaa.org.au");
+        expect(data["subscribe_to_updates"]).toBe(true);
+        expect(data["subscribe_to_newsletter"]).toBe(false);
+        expect(data["connect_to_community"]).toBe(false);
+        expect(data["how_did_you_hear_about_us_db"]).toBe("cant-remember");
+        expect(data["form-TOTAL_FORMS"]).toBe(2);
+        expect(data["form-INITIAL_FORMS"]).toBe(2);
+        expect(data["form-0-id"]).toBe(null);
+        expect(data["form-0-partner_charity"]).toBe("unallocated");
+        expect(data["form-0-amount"]).toBe("50");
+        expect(data["form-1-id"]).toBe(null);
+        expect(data["form-1-partner_charity"]).toBe("eaa-amplify");
+        expect(data["form-1-amount"]).toBe("15.00");
+
+        // Make sure things that shouldn't be sent are not sent
+        expect(data["is_gift"]).toBe(undefined);
+        expect(data["gift_recipient_name"]).toBe(undefined);
+        expect(data["gift_recipient_email"]).toBe(undefined);
+        expect(data["gift_personal_message"]).toBe(undefined);
+        expect(data["form-2-id"]).toBe(undefined);
+        expect(data["form-2-partner_charity"]).toBe(undefined);
+        expect(data["form-2-amount"]).toBe(undefined);
+
+        resolve();
+      }
+    });
+  });
+
+  await page.getByRole("button", { name: "Donate" }).click();
+
+  await testFinished;
+});
